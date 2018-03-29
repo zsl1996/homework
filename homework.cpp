99ee5e4c2c6f8c34e2ad1a509f860814bc7cf16d
zsl1996 <2392587229@qq.com>
2018-03-29 17:58:25 +0800

    版本2.0
    大改了逻辑，修复单词少于十个个bug，速度提升到26s

--- homework.cpp
+++ homework.cpp
@@ -5,11 +5,13 @@
 #include <iostream>  
 #include<unordered_map>
 #include <valarray>  
+#include <time.h>
 #include<iterator>
 #include<bitset>
 #include<string.h>
 using namespace std;
 string topworld[10];
+	time_t timeBegin, timeEnd;
 int wnumtop[10] = { 0 };
 int allch = 0; //the number of char
 int allline = 0; //the num of line
@@ -79,28 +81,14 @@ void GetAllFiles(string path, vector<string>& files)
 }
 #endif
 
-
-
-bitset<20> bitcmp(bitset<20> a, bitset<20> b) { //按字典顺序返回正确的词
-	for (int j = 0; j < 20; j++) {
-		if (a.test(j) > b.test(j)) {
-			return a;
-		}
-		if (a.test(j)< b.test(j)) {
-			return b;
-		}
-	}
-	return a;
-}
-
-int ReadByChar(string file, int &fch, int &line, unordered_map<string, int> &word, unordered_map<string, int> &phrase, unordered_map<string, bitset<20>> &wordd, unordered_map<string, string>& wordend)
+int ReadByChar(string file, int &fch, int &line, unordered_map<string, int> &word, unordered_map<string, int> &phrase, unordered_map<string, string> &wordend)
 {
-	bitset<20> q = 0;
 	fstream openbychar;
 	char c;
 	int flag = 0;//标志是否是第一个词
 	openbychar.open(file, ios::in);
-	string sword = "";
+	string sword = ""; //存小写
+	string bsword = ""; //存大小写
 	string sphrase = "";
 	string num = "";
 	unordered_map<string, string>::iterator it;
@@ -117,121 +105,108 @@ int ReadByChar(string file, int &fch, int &line, unordered_map<string, int> &wor
 		//统计单词
 		if ((c < 91 && c>64) || (c < 123 && c>96)) { //字母
 			flag++;
+			bsword.push_back(c); 
 			if (c < 91 && c>64) // 范围大概已知用位运算简化11111111111111111111111111111111
 			{
 				c = c + 32;
-				sword = sword + c;
-				if (flag<20) q.set(flag - 1);
+				sword.push_back(c);
 			}
 			//			else sword = sword + c; //  优化！！！！！！！
 			else sword.push_back(c); //  优化！！！！！！！
 		}
 		else if (c > 47 && c < 58) { //数字   !!!!!!!!!!!file123 与file
 			if (flag > 3) {
-				num = num + c;
-				sword = sword + c;
+				num.push_back(c);
+				bsword.push_back(c);
+				sword.push_back(c);
 				flag++;
 			}
 			else {
 				flag = 0;
 				sword = "";
-				q = 0;
+				bsword = "";
 				num = "";
 			}
 		}
 		else {   //特殊字符
 			if (flag > 3) {
-				if (sword[sword.length() - 1] < 123 && sword[sword.length() - 1]>96) //最后是字母
+				if (sword[flag - 1] < 123 && sword[flag - 1]>96) //最后是字母
 				{
 					word[sword]++;
-				 bitset<20> a = bitcmp(q, wordd[sword]);
-					if (q == a) wordend[sword] = "";//有纯单词，且按字典顺序靠前，只需要存后缀为空
-					wordd[sword] = a;
-					sphrase = sphrase + " " + sword; //第一个词组有点bug，可以输出的时候去掉！！！！！！！！！
+					string s = wordend[sword];
+					if (wordend[sword] == "") { //第一次读入
+						wordend[sword]=bsword;
+					}
+					else {
+						wordend[sword] = s.compare(bsword) > 0 ? bsword : s;
+					}
+					sphrase = sphrase + " " + sword; //可以优化！！！！！！！！！！！！！
 					phrase[sphrase]++;
 					sphrase = sword;
 				}
 				else { //后缀为数字
-
 					sword = sword.substr(0, strlen(sword.c_str()) - strlen(num.c_str())); // 优化！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
 					word[sword]++;
-				 bitset<20> c = wordd[sword];
-					if (q == c) {
-						if (wordend[sword] != "") {
-							wordend[sword] = wordend[sword].compare(num) > 0 ? num : wordend[sword];
-						}
+					string s = wordend[sword];
+					if (wordend[sword] == "") { //第一次读入
+						wordend[sword] = bsword;
 					}
-					else if(q == bitcmp(q,c))
-					{
-						wordend[sword] = num;
-						wordd[sword] = q;
-					}	
+					else {
+						wordend[sword] = s.compare(bsword) > 0 ? bsword : s;
+					}
 					sphrase = sphrase + " " + sword; //第一个词组有点bug，可以输出的时候去掉！！！！！！！！！！！
 					phrase[sphrase]++;
 					sphrase = sword;
 				}
 				sword = "";
+				bsword = "";
 				num = "";
-				q = 0;
 				flag = 0;
 			}
 			else {
 				flag = 0;
 				sword = "";
+				bsword = "";
 				num = "";
-				q = 0;
 			}
 		}
 	}
 	if (sword.length() > 3) { //文末最后一个单词//bug!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		if (flag > 3) {
-			if (sword[sword.length() - 1] < 123 && sword[sword.length() - 1]>96) //最后是字母
-			{
-				word[sword]++;
-				 bitset<20> d = bitcmp(q, wordd[sword]);
-				if (q == d ) wordend[sword] = "";//有纯单词，且按字典顺序靠前，只需要存后缀为空
-				wordd[sword] = d;
-				sphrase = sphrase + " " + sword; //第一个词组有点bug，可以输出的时候去掉！！！！！！！！！
-				phrase[sphrase]++;
-				sphrase = sword;
+		if (sword[flag - 1] < 123 && sword[flag - 1]>96) //最后是字母
+		{
+			word[sword]++;
+			string s = wordend[sword];
+			if (wordend[sword] == "") { //第一次读入
+				wordend[sword] = bsword;
 			}
-			else { //后缀为数字
-
-				sword = sword.substr(0, strlen(sword.c_str()) - strlen(num.c_str())); // 优化！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
-				word[sword]++;
-				 bitset<20> e = wordd[sword];
-				if (q == e) {
-					if (wordend[sword] != "") {
-						wordend[sword] = wordend[sword].compare(num) > 0 ? num : wordend[sword];
-					}
-				}
-				else if (q == bitcmp(q, e))
-				{
-					wordend[sword] = num;
-					wordd[sword] = q;
-				}
-				sphrase = sphrase + " " + sword; //第一个词组有点bug，可以输出的时候去掉！！！！！！！！！！！
-				phrase[sphrase]++;
-				sphrase = sword;
+			else {
+				wordend[sword] = s.compare(bsword) > 0 ? bsword : s;
 			}
-			sword = "";
-			num = "";
-			q = 0;
-			flag = 0;
+			sphrase = sphrase + " " + sword; //可以优化！！！！！！！！！！！！！
+			phrase[sphrase]++;
+			sphrase = sword;
 		}
-		sword = "";
-		num = "";
-		q = 0;
-		flag = 0;
+		else { //后缀为数字
+			sword = sword.substr(0, strlen(sword.c_str()) - strlen(num.c_str())); // 优化！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+			word[sword]++;
+			string s = wordend[sword];
+			if (wordend[sword] == "") { //第一次读入
+				wordend[sword] = bsword;
+			}
+			else {
+				wordend[sword] = s.compare(bsword) > 0 ? bsword : s;
+			}
+			sphrase = sphrase + " " + sword; //第一个词组有点bug，可以输出的时候去掉！！！！！！！！！！！
+			phrase[sphrase]++;
+			sphrase = sword;
+		}
 	}
-	q = 0;
 	openbychar.close();
 	if (fch == 0) line = 0;
 	return 0;
 }
 
 void SplitString(string& s, vector<std::string>& v)
-
 {
 	const std::string& c = " ";
 	//优化！！！！！！！！！！！！！！！！！！！！！！
@@ -290,24 +265,25 @@ void top(unordered_map<string, int> & word) {
 
 }
 
-void writewordtxt(string filepath, unordered_map<string, bitset<20> >& wordd, unordered_map<string, string> wordend) { //输出到txt
+void writewordtxt(string filepath, unordered_map<string, string> wordend) { //输出到txt
 	fstream writebychar;
 	int max = 0;
 	writebychar.open(filepath, ios::app);
 	for (int i = 0; i < 10; i++)
 	{
 		max = getmax();
-		string s = worddtostr(wordd, topworld[max]);
-		writebychar << (s + wordend[topworld[max]]); //优化！！！！！！！！！！！！！！！！！！！！
-		writebychar << " ";
-		writebychar << wnumtop[max] << "\n";
-		wnumtop[max] = 0;
+		if (wnumtop[max] != 0) {
+			writebychar << (wordend[topworld[max]]);
+			writebychar << " ";
+			writebychar << wnumtop[max] << "\n";
+			wnumtop[max] = 0;
+		}
 	}
 	writebychar << "\n\n";
 	writebychar.close();
 }
 
-void writeworddtxt(string filepath, unordered_map<string, bitset<20>> wordd, unordered_map<string, string> &wordend) { // 等待修改！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
+void writeworddtxt(string filepath, unordered_map<string, string> &wordend) { // 等待修改！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！！
 	fstream writebychar;
 	int max = 0;
 	writebychar.open(filepath, ios::app);
@@ -316,13 +292,16 @@ void writeworddtxt(string filepath, unordered_map<string, bitset<20>> wordd, uno
 		max = getmax();
 		vector<string> s;
 		SplitString(topworld[max], s);
-		writebychar << (worddtostr(wordd, s[0]) + wordend[s[0]]) << " " << (worddtostr(wordd, s[1]) + wordend[s[1]]);
+		if (s[0] != ""&&wnumtop[max] != 0){
+		writebychar << wordend[s[0]] << " " << wordend[s[1]];
 		writebychar << " ";
 		writebychar << wnumtop[max] << "\n";
 		wnumtop[max] = 0;
-
+		}
 	}
 	writebychar << "\n\n";
+	//timeEnd = time(NULL);
+	//writebychar << timeEnd - timeBegin;
 	writebychar.close();
 }
 
@@ -347,8 +326,9 @@ void writecharnum(string path, unordered_map<string, int> word) {
 
 int main(int argc, char *argv[])
 {	//"C:/Users/zsl/Desktop/test/newsample"
+
+	//timeBegin = time(NULL);
 	unordered_map<string, int> word; //单词 数目
-	unordered_map<string, bitset<20>> wordd;//单词 对应应该大写的位
 	unordered_map<string, int> phrase; //词组 数目
 	unordered_map<string, string> wordend; //词 可能有的单词后缀
 	vector<string> files;
@@ -361,27 +341,25 @@ int main(int argc, char *argv[])
 	if (s.find(".") == string::npos) {
 		filepath = s;
 		GetAllFiles(filepath, files);
-
 		int size = files.size();
 		for (int i = 0; i < size; i++)
 		{
 			int fch = 0;
 			int line = 0;
-			ReadByChar(files[i], fch, line, word, phrase, wordd, wordend);
+			ReadByChar(files[i], fch, line, word, phrase,wordend);
 			allch += fch;
 			allline += line;
 		}
 	}
 	else {
-		ReadByChar(s, allch, allline, word, phrase, wordd, wordend);
+		ReadByChar(s, allch, allline, word, phrase,wordend);
 	}
-
 	writecharnum(writepath, word);
 	if (allch != 0) {
 		top(word);
-		writewordtxt(writepath, wordd, wordend);
+		writewordtxt(writepath, wordend);
 		top(phrase);
-		writeworddtxt(writepath, wordd, wordend);
+		writeworddtxt(writepath, wordend);
 	}
-	return 0;
+return 0;
 }
